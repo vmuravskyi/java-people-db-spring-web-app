5e1a178c84cd75cbfbc5c66f3bafe58574de2278
v.muravskyi <v.muravskyi@gmail.com>
2022-08-29 11:04:37 +0300

    save

--- src/main/java/com/muravskyi/peopledbweb/PeopledbWebApplication.java
+++ src/main/java/com/muravskyi/peopledbweb/PeopledbWebApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PeopledbWebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PeopledbWebApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PeopledbWebApplication.class, args);
+    }
 
 }
--- src/main/java/com/muravskyi/peopledbweb/data/PersonDataLoader.java
+++ src/main/java/com/muravskyi/peopledbweb/data/PersonDataLoader.java
@@ -1,13 +1,14 @@
 package com.muravskyi.peopledbweb.data;
 
 import com.muravskyi.peopledbweb.biz.model.Person;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class PersonDataLoader implements ApplicationRunner {
 
     private PersonRepository personRepository;
@@ -18,16 +19,18 @@ public class PersonDataLoader implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        List<Person> people = List.of(
-                new Person(10L, "Jake", "Snake", LocalDate.of(1950, 1, 1), new BigDecimal("70000")),
-                new Person(20L, "Sarah", "Snake", LocalDate.of(1960, 2, 1), new BigDecimal("15000")),
-                new Person(30L, "Jake", "Smith", LocalDate.of(1970, 3, 1), new BigDecimal("120000")),
-                new Person(40L, "Johnny", "Jackson", LocalDate.of(1980, 4, 1), new BigDecimal("25000")),
-                new Person(40L, "Jane", "Smith", LocalDate.of(1997, 5, 1), new BigDecimal("25000")),
-                new Person(40L, "Johnny", "Jackson", LocalDate.of(1980, 6, 1), new BigDecimal("25000")),
+        if (personRepository.count() == 0) {
+            List<Person> people = List.of(
+                new Person(10L, "Pete", "Snake", LocalDate.of(1950, 1, 1), new BigDecimal("70000")),
+                new Person(20L, "Jennifer", "Snake", LocalDate.of(1960, 2, 1), new BigDecimal("15000")),
+                new Person(30L, "Mark", "Smith", LocalDate.of(1970, 3, 1), new BigDecimal("120000")),
+                new Person(40L, "Vishnu", "McGuire", LocalDate.of(1980, 4, 1), new BigDecimal("25000")),
+                new Person(40L, "Alice", "Smith", LocalDate.of(1997, 5, 1), new BigDecimal("25000")),
+                new Person(40L, "Akira", "Jackson", LocalDate.of(1980, 6, 1), new BigDecimal("25000")),
                 new Person(50L, "Bobby", "Kim", LocalDate.of(1990, 11, 1), new BigDecimal("35000"))
-        );
-        personRepository.saveAll(people);
+            );
+            personRepository.saveAll(people);
+        }
     }
 
 }
